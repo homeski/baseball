d8eb12f3313aa2b13d0214a03646e377b9beaa0d
homeski <homeski2@gmail.com>
2017-04-20 00:49:25 -0400

    added simple rating function

--- index.js
+++ index.js
@@ -10,14 +10,10 @@ function getGames(teams) {
 
   	schedule.dailygameschedule.gameentry.forEach(function(entry) {
   		games[entry.id] = {
-			away: 		teams[entry.awayTeam.ID].name,
-			awayWin:		teams[entry.awayTeam.ID].win,
-			awayLoss:	teams[entry.awayTeam.ID].loss,
-			home: 		teams[entry.homeTeam.ID].name,
-			homeWin:	 	teams[entry.homeTeam.ID].win,
-			homeLoss: 	teams[entry.homeTeam.ID].loss,
+			awayID: 		entry.awayTeam.ID,
+			homeID: 		entry.homeTeam.ID,
 			loc: 			entry.location
-		}
+		};
 	});
 
 	return games;
@@ -30,20 +26,49 @@ function getStandings() {
 		teams[entry.team.ID] = {
 			city: entry.team.City,
 			name: entry.team.Name,
-			win:	entry.stats.Wins['#text'],
-			loss: entry.stats.Losses['#text']
+			win:	parseInt(entry.stats.Wins['#text']),
+			loss: parseInt(entry.stats.Losses['#text']),
+			pct:  entry.stats.WinPct['#text'],
+			rs:   parseInt(entry.stats.RunsFor['#text']),
+			ra:	parseInt(entry.stats.RunsAgainst['#text'])
       };
   	});
 
   	return teams;
 }
 
+
+// returns true if better team (by wins)
+// has also scored more runs on the season
+//
+// return false otherwise or if teams have same wins
+function rateMatchup(team1, team2) {
+	var fav = team1.win >= team2.win ? team1 : team2;
+	var dog = team1.win >= team2.win ? team2 : team1;
+
+	if (fav.win == dog.win)
+		return false;
+	
+	if (fav.rs > dog.rs)
+		return true;
+
+	return false;
+}
+
 var teams = getStandings();
 var games = getGames(teams);
 
 for (var k in games) {
-	var output = Mustache.render("({{awayWin}}-{{awayLoss}}) {{away}} @ {{home}} ({{homeWin}}-{{homeLoss}})", games[k]);
-	console.log(output);
+   var render = {
+		away: 	teams[games[k].awayID],
+		home: 	teams[games[k].homeID],
+		loc:  	games[k].loc
+   };
+	
+	if (rateMatchup(render.away, render.home)) {
+		var output = Mustache.render("({{away.win}}-{{away.loss}} {{away.rs}}) {{away.name}} @ {{home.name}} ({{home.win}}-{{home.loss}} {{home.rs}})", render);
+		console.log(output);
+	}
 }
 
 
