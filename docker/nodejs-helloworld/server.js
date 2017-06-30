733e7c9c0ff29c7494b784a136aea229e67b718e
Homero Pawlowski <homeski2@gmail.com>
2017-06-30 03:48:03 +0000

    now matches on substr

--- docker/nodejs-helloworld/server.js
+++ docker/nodejs-helloworld/server.js
@@ -6,6 +6,17 @@ var request = require('request');
 var fs = require('fs');
 var shell = require('shelljs');
 
+const DEBUG = false;
+var bot_id, group_id;
+
+if (DEBUG) {
+  bot_id = '66782dd3a2d5645feca4718af8';
+  group_id = '32060693';
+} else {
+  bot_id = '4b84658985fbdc082076840e85';
+  group_id = '13897405';
+}
+
 // request
 var msg_options = {
     url: 'https://api.groupme.com/v3/bots/post?token=0RBWlSjAzqMbCApZl3hLRGl1CP2UqWRPeSQlseGn',
@@ -13,8 +24,7 @@ var msg_options = {
     headers: {
       'User-Agent': 'Super Agent/0.0.1',
       'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    form: {'bot_id': '66782dd3a2d5645feca4718af8', 'group_id': '32060693', 'text': 'yes?'}
+    }
 };
 //
 
@@ -37,12 +47,12 @@ app.post('/groupme', function (req, res) {
 
   var matchStr = 'MACBOT';
 
-  if (req.body['text'].trim().toUpperCase() === matchStr) {
+  if (req.body['text'].trim().toUpperCase().indexOf(matchStr) !== -1) {
     var response = shell.exec("curl -s 'https://image.groupme.com/pictures' -X POST -H 'X-Access-Token: 0RBWlSjAzqMbCApZl3hLRGl1CP2UqWRPeSQlseGn' -H 'Content-Type: image/jpeg' --data-binary @./photos/`ls photos | shuf -n 1`").stdout;
     var img_url = JSON.parse(response).payload.url;
     console.log(img_url);
 
-    msg_options.form = {'bot_id': '4b84658985fbdc082076840e85', 'group_id': '13897405', 'picture_url': img_url}
+    msg_options.form = {'bot_id': bot_id, 'group_id': group_id, 'picture_url': img_url}
 
     // respond yes?
     request(msg_options, function (error, response, body) {
